c2133a969a498592b7139cedd4063b74580e2c60
Richard Harvey <theerichardharvey@gmail.com>
2022-06-17 14:49:45 -0400

    Summary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Movies from './components/movies';
 import './App.css';
 
+// class component
 class App extends Component {
   render() {
     return (
