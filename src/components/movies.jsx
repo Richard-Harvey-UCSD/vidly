d1087b49349c5deca1f0379631f86039448765ef
Richard Harvey <theerichardharvey@gmail.com>
2022-06-17 14:08:43 -0400

    Like Component Exercise

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,6 +16,7 @@ class Movies extends Component {
   render() {
     const { length: count } = this.state.movies;
 
+    // check for movies in the database
     if (count === 0) return <p>There are no movies in the database.</p>;
 
     return (
