3f656abe8a5aec055c34f96e8a00eb9d9b8c3381
Richard Harvey <theerichardharvey@gmail.com>
2022-06-17 15:10:50 -0400

    Pagination - Displaying Pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from "react";
+import _ from "lodash"; // optimized from JS library underscore
 
 const Pagination = (props) => {
-  return null;
+  const { itemsCount, pageSize } = props;
+
+  // [1, ..., pagesCount].map()
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  console.log("pagesCount: ", pagesCount);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map((page) => (
+          <li key={page} className="page-item">
+            <a className="page-link">{page}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
