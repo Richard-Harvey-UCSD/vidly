1b07ee6048a905b59df76e9494c9fe933b9e2e35
Richard Harvey <theerichardharvey@gmail.com>
2022-06-18 18:22:20 -0400

    Refs

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef();
+
+  // componentDidMount() {
+  //   this.username.current.focus();
+  // }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
     // call the server
+    const username = this.username.current.value;
     console.log("Submitted");
   };
 
@@ -15,7 +22,13 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
