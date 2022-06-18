dd994903f9e572a1eeaa5e272d0e8233d493cb64
Richard Harvey <theerichardharvey@gmail.com>
2022-06-18 17:10:37 -0400

    Adding the NavBar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
-import "./App.css";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
-import NotFound from "./components/notfound";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import "./App.css";
 
 // class component
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        {/* <Movies /> */}
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={NotFound}></Route>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
