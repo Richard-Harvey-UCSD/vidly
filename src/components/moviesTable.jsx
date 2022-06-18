0abcb09591ca9a0ab181daec7df48f1972b34459
Richard Harvey <theerichardharvey@gmail.com>
2022-06-18 10:04:34 -0400

    Sorting - Extracting TableBody

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -22,6 +22,7 @@ class MoviesTable extends Component {
           sortColumn={sortColumn}
           onSort={onSort}
         />
+        <TableBody data={movies} />
         <tbody>
           {movies.map((movie) => (
             <tr key={movie._id}>
