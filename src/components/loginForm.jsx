c5d7ceca6072cf9ec42c2b02c7b0dc98d3f9be47
Richard Harvey <theerichardharvey@gmail.com>
2022-06-19 01:20:02 -0400

    Validating a Form Using Joi

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -10,8 +10,8 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required(),
+    username: Joi.string().required().label("Username"),
+    password: Joi.string().required().label("Password"),
   };
 
   // username = React.createRef();
@@ -21,20 +21,26 @@ class LoginForm extends Component {
   // }
 
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false,
-    });
-    console.log("result: ", result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+
+    if (!error) return null;
 
     const errors = {};
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
 
-    const { account } = this.state;
-    if (account.username.trim() === "")
-      errors.username = "Username is required.";
-    if (account.password.trim() === "")
-      errors.password = "Password is required.";
+    // console.log("result: ", result);
 
-    return Object.keys(errors).length === 0 ? null : errors;
+    // const errors = {};
+
+    // const { account } = this.state;
+    // if (account.username.trim() === "")
+    //   errors.username = "Username is required.";
+    // if (account.password.trim() === "")
+    //   errors.password = "Password is required.";
+
+    // return Object.keys(errors).length === 0 ? null : errors;
   };
 
   validateProperty = ({ name, value }) => {
