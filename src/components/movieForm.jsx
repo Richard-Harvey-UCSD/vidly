070d3ac3e9875ed0302a5ef61fd97cae6ba2f714
Richard Harvey <theerichardharvey@gmail.com>
2022-06-22 19:39:21 -0400

    Refactoring

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -34,20 +34,26 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
