79eeab23b52392f8285ae8d3dd5370f03b7fffb6
Richard Harvey <theerichardharvey@gmail.com>
2022-06-22 19:03:03 -0400

    Populating the Form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getMovie, saveMovie } from "../services/movieService";
+// import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/genreService";
+// import { getGenres } from "../services/fakeGenreService";
 
 class MovieForm extends Form {
   state = {
@@ -32,17 +34,20 @@ class MovieForm extends Form {
       .label("Daily Rental Rate"),
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async componentDidMount() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
 
     const movieId = this.props.match.params.id;
     if (movieId === "new") return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
   }
 
   mapToViewModel(movie) {
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -7,6 +7,13 @@ export function getMovies() {
   return http.get(apiEndpoint);
 }
 
+export function getMovie(movieId) {
+  return http.get(apiEndpoint + '/' + movieId);
+}
+
+export function saveMovie(movie) {
+}
+
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + '/' + movieId);
 }
