43d5fc05874a63c31065e464422019c9ed54c288
Richard Harvey <theerichardharvey@gmail.com>
2022-06-23 11:35:15 -0400

    Handling Login Errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -19,8 +19,16 @@ class LoginForm extends Form {
   doSubmit = async () => {
     // call the server
     // console.log("Submitted");
-    const { data } = this.state;
-    await login(data.username, data.password);
+    try {
+      const { data } = this.state;
+      await login(data.username, data.password);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
