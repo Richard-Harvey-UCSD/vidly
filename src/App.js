a107f63f1800f9072f431793dc72e238b47491cf
Richard Harvey <theerichardharvey@gmail.com>
2022-06-24 09:54:58 -0400

    Showing or Hiding Elements based on the User

--- src/App.js
+++ src/App.js
@@ -30,14 +30,17 @@ class App extends Component {
         <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
-            <Route path="/register" component={RegisterForm}></Route>
-            <Route path="/movies/:id" component={MovieForm}></Route>
-            <Route path="/login" component={LoginForm}></Route>
-            <Route path="/logout" component={Logout}></Route>
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound}></Route>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route
+              path="/movies"
+              render={props => <Movies {...props} user={this.state.user} />}
+            />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
             <Redirect from="/" exact to="/movies" />
             <Redirect to="/not-found" />
           </Switch>
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -111,12 +111,13 @@ class Movies extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, sortColumn } = this.state;
+    const { pageSize, currentPage, sortColumn, searchQuery } = this.state;
+    const { user } = this.props;
 
     // check for movies in the database
     if (count === 0) return <p>There are no movies in the database.</p>;
 
-    const { totalCount, data: movies, searchQuery } = this.getPagedData();
+    const { totalCount, data: movies } = this.getPagedData();
 
     return (
       <div className="row">
@@ -130,13 +131,15 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <Link
-            to="/movies/new"
-            className="btn btn-primary"
-            style={{ marginBottom: 20 }}
-          >
-            New Movie
-          </Link>
+          {user && (
+            <Link
+              to="/movies/new"
+              className="btn btn-primary"
+              style={{ marginBottom: 20 }}
+            >
+              New Movie
+            </Link>
+          )}
           <p>Showing {totalCount} movies in the database.</p>
           <SearchBox values={searchQuery} onChange={this.handleSearch} />
           <MoviesTable
